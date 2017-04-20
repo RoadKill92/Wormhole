ce9d7357d5002820963c63b418b469f7d182ee6f
Tannis Goodman <Tannis Goodman>
2017-04-20 01:30:48 -0400

    Initial commit

---
+++ ButtonGame.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace ButtonGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCurrent = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(23, 30);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(80, 34);
+            this.btnPlay.TabIndex = 0;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(290, 30);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(81, 34);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(23, 82);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(90, 85);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "?";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(129, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 34);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Click boxes below to find button.";
+            // 
+            // lblCurrent
+            // 
+            this.lblCurrent.AutoSize = true;
+            this.lblCurrent.Location = new System.Drawing.Point(20, 296);
+            this.lblCurrent.Name = "lblCurrent";
+            this.lblCurrent.Size = new System.Drawing.Size(133, 16);
+            this.lblCurrent.TabIndex = 9;
+            this.lblCurrent.Text = "Points Remaing: 0";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(169, 296);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(210, 16);
+            this.lblTotal.TabIndex = 10;
+            this.lblTotal.Text = "Total Score: 0 out of 0 games";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(150, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 85);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "?";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(281, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 85);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "?";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(23, 193);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(90, 85);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "?";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(150, 193);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(90, 85);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "?";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.White;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(281, 193);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(90, 85);
+            this.button6.TabIndex = 15;
+            this.button6.Text = "?";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 321);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblCurrent);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnPlay);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Tannis\' Button Game";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCurrent;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ ButtonGame.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ButtonGame
+{
+    public partial class Form1 : Form
+    {
+        public const string BELL_FILE = "Bell.wav";
+        public const string BUTTON_IMAGE_FILE = "buttonImage.jpg";
+        List<Button> buttonList; // This will hold a list of all your "?" button controls
+        int hidingPlace; // Button number where your sewing buttons are hidden
+        int currentScore; // Current score for (the current round of play
+        int totalScore; // This will hold the total score
+        int gameCount; // Number of games that have been played
+        System.Media.SoundPlayer bell; // Media player to play the sound of a bell
+        Bitmap picture; // Picture of your sewing buttons.
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buttonList = new List<Button>();
+// Go through the form’s controls and find all the "?" button controls
+// Placing them in your list.
+foreach (Control item in this.Controls)
+{
+if ((string)item.Text == "?")
+{
+buttonList.Add((Button)item);
+}
+}
+// Get file path that this executable loaded from
+string appPath = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+// Create file path to sound and image files
+string bellFilePath = System.IO.Path.Combine(appPath, BELL_FILE);
+string pictureFilePath = System.IO.Path.Combine(appPath, BUTTON_IMAGE_FILE);
+// Load bell sound file and sewing buttons picture file
+bell = new System.Media.SoundPlayer(bellFilePath);
+picture = new Bitmap(pictureFilePath);
+// initialize counts
+currentScore = 0;
+totalScore = 0;
+gameCount = 0;
+hidingPlace = 0;
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            // Clear out button image from last game
+            if (buttonList[hidingPlace] != null)
+            {
+                buttonList[hidingPlace].Image = null;
+                buttonList[hidingPlace].Refresh();
+            }
+            // Flash the "?" buttons randomly with Hot Pink
+            // At end of loop, hidingPlace will always have
+            // index number of last button flashed.
+            for (int i = 1; i <= 24; i++) // flash 24 times
+            {
+                Random dice = new Random(); // Random number generator
+                Color previous;
+                hidingPlace = dice.Next(1, 600) % 6; // Number between 0 and 5 inclusive
+                previous = buttonList[hidingPlace].BackColor; // Save original color
+                buttonList[hidingPlace].BackColor = Color.HotPink; // paint it Hot Pink
+                buttonList[hidingPlace].Refresh();
+                System.Threading.Thread.Sleep(20); // 20ms is fastest a human can respond
+                buttonList[hidingPlace].BackColor = previous; // restore previous color
+                buttonList[hidingPlace].Refresh();
+            }
+            gameCount = gameCount + 1; // increment gameCount
+            currentScore = 6; // reset guess score 
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int p;
+            Button b;
+            // Get the button that was clicked and see if it is the hiding place
+            b = (Button)sender;
+            p = buttonList.FindIndex(b.Equals);
+            if (p == hidingPlace)
+            {
+                totalScore = totalScore + currentScore;
+                b.Image = picture;
+                bell.Play();
+            }
+            else
+            {
+                if (currentScore > 0) currentScore = currentScore - 1;
+            }
+            // Format and display score information
+            lblCurrent.Text = String.Format("Points Remaining: {0:N0}", currentScore);
+            lblTotal.Text = String.Format("Total Score: {0:N0} out of { 1:N0} games", totalScore, gameCount);
+            lblCurrent.Refresh();
+            lblTotal.Refresh();
+        }
+    }
+}
